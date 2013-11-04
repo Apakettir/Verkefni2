bf1e6bc2ab28039708a0917bcbb048b3bf2a800f
FreyrGudna <freyr123@gmail.com>
2013-11-04 18:06:54 +0000

    Koda breytt fyrir eina tolu

--- src/main/java/is/ru/stringCalc/Calculator.java
+++ src/main/java/is/ru/stringCalc/Calculator.java
@@ -6,12 +6,12 @@ public class Calculator{
     if(numbers == ""){
       return 0;
     }
-    else if(numbers.contains(",")){
+    //else if(numbers.contains(",")){
       String[] Numbers = numbers.split(",");
       int heild = Integer.parseInt(Numbers[0]) + Integer.parseInt(Numbers[1]);
       return heild; 
-    }
-    return 1;
+    //}
+    //return 1;
   }
 
 }
